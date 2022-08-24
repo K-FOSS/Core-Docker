8544b7a1659a92636cd6e0a9463a1295b0f6602c
Kristian Jones <me@kristianjones.dev>
2022-08-24 15:25:08 +0000

    fix

--- Images/Kea/Healthcheck.sh
+++ Images/Kea/Healthcheck.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-status=$(keactrl status | grep "DHCPv4 server" | awk '{print $3}')
+status=$(keactrl status -c /etc/DHCP/keactrl.conf | grep "DHCPv4 server" | awk '{print $3}')
 
 if [[ "${status}" == "active" ]]; then
   exit 0
