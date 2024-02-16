6b365452d4b26e1a0f958f3fb10b8519645fc6c5
kjones <kristine@mylogin.space>
2024-02-16 02:59:27 +0000

    fix

--- Images/FreeSwitch/entrypoint.sh
+++ Images/FreeSwitch/entrypoint.sh
@@ -1,29 +1,25 @@
+#!/bin/sh
 
-if [ "$1" = 'freeswitch' ]; then
-    BASEURL=https://files.freeswitch.org/releases/sounds/
-    PID_FILE=/var/run/freeswitch/freeswitch.pid
 
-    if [ "$EPMD" = "true" ]; then
-        /usr/bin/epmd -daemon
-    fi
+BASEURL=https://files.freeswitch.org/releases/sounds/
+PID_FILE=/var/run/freeswitch/freeswitch.pid
 
-    chown -R freeswitch:freeswitch /etc/freeswitch
-    chown -R freeswitch:freeswitch /var/lib/freeswitch
-    chown -R freeswitch:freeswitch /var/run/freeswitch
-    chown -R freeswitch:freeswitch /var/log/freeswitch
-
-    trap '/usr/bin/freeswitch -stop' TERM
-
-    if [ -d /docker-entrypoint.d ]; then
-        for f in /docker-entrypoint.d/*.sh; do
-            [ -f "$f" ] && . "$f"
-        done
-    fi
-    su-exec freeswitch:freeswitch /usr/bin/freeswitch -nonat -c &
-    pid="$!"
-
-    wait $pid
-    exit 0
+if [ "$EPMD" = "true" ]; then
+    /usr/bin/epmd -daemon
 fi
 
-exec "$@"
+
+trap '/usr/bin/freeswitch -stop' TERM
+
+if [ -d /docker-entrypoint.d ]; then
+    for f in /docker-entrypoint.d/*.sh; do
+        [ -f "$f" ] && . "$f"
+    done
+fi
+su-exec freeswitch:freeswitch /usr/bin/freeswitch -nonat -c &
+pid="$!"
+
+wait $pid
+exit 0
+
+
