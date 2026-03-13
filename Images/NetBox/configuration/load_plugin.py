d7253bd480ff12fcc9e9b9685bdcdfb7021bc625
Entrapta Jones <august@mylogin.space>
2026-03-13 18:18:04 +0000

    fix(Images.Netbox): Work on implementing more of the Netbox plugins

--- Images/NetBox/configuration/load_plugin.py
+++ Images/NetBox/configuration/load_plugin.py
@@ -1,4 +1,4 @@
-PLUGINS = ['netbox_ipcalculator', 'netbox_topology_views']
+PLUGINS = ['netbox_ipcalculator', 'netbox_topology_views', 'netbox_lists']
 
 PLUGINS_CONFIG = {
     'netbox_ipcalculator': {
